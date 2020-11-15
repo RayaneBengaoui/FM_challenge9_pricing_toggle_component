36e0456e225305a812d6c59f105f638900af9530
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2020-11-15 13:20:04 +0100

    Adding toggle from monthly/yearly prices

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const toggleContainer = document.querySelector(".toggle-container");
-const toggleCircle = document.querySelector(".inner-circle");
+
+const price_1 = document.querySelector(".price_1");
+const price_2 = document.querySelector(".price_2");
+const price_3 = document.querySelector(".price_3");
+const prices = [price_1, price_2, price_3];
 
 toggleContainer.addEventListener("click", () => {
   toggleContainer.classList.toggle("active");
-  console.log("work");
+
+  prices.map((price) => {
+    let string = price.innerHTML;
+    if (toggleContainer.classList.contains("active")) {
+      price.innerHTML = string.slice(0, 3) + "9" + string.slice(3, 6);
+    } else {
+      price.innerHTML = string.slice(0, 3) + "" + string.slice(4, 7);
+    }
+  });
 });
